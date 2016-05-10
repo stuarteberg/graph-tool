stuarteberg/graph-tool

commit 2c87718697606208e9be2fadff96b46897f1dced

Workaround for clang bug

src/graph/community_old/graph_blockmodel_overlap.cc
@@ -693,7 +693,7 @@ struct get_augmented_overlap
         for (auto v : vertices_range(g))
         {
             size_t vi = node_index[v];
-            auto br = std::make_tuple(b[v], vi);
+            auto br = std::make_tuple(int(b[v]), int(vi));
             size_t idx;
             auto iter = idx_map.find(br);
             if (iter != idx_map.end())
@@ -704,7 +704,6 @@ struct get_augmented_overlap
             {
                 idx = pos;
                 idx_map[br] = pos++;
-                idx_rmap.push_back(br);
             }
             br_map[v] = idx;
         }

src/graph/hash_map_wrap.hh
@@ -341,6 +341,18 @@ public:
     void resize(size_t n) { base_t::reserve(n); }
 #endif
 
+#if !defined(HAVE_SPARSEHASH) && defined(__clang__)
+    // this is a workaround for a bug in clang:
+    // https://llvm.org/bugs/show_bug.cgi?id=24770
+    // https://llvm.org/bugs/show_bug.cgi?id=14858
+    Value& operator[](const Key& key )
+    {
+        auto iter = this->find(key);
+        if (iter == this->end())
+            iter = this->insert({key, Value()}).first;
+        return iter->second;
+    }
+#endif
 };
 
 #endif // HASH_MAP_WRAP_HH

commit 104e09acca83fd8ad8c98639232d3d13b1d4d548

Fix bug introduced in commit 2c877186976

src/graph/community_old/graph_blockmodel_overlap.cc
@@ -704,6 +704,7 @@ struct get_augmented_overlap
             {
                 idx = pos;
                 idx_map[br] = pos++;
+                idx_rmap.push_back(br);
             }
             br_map[v] = idx;
         }